00a79fb2e01f5d37fdc161df128754f60ca33b67
Ramadhan2212 <ramadhan72679@gmail.com>
2024-11-26 14:31:02 +0800

    Add files via upload

--- Modul 4/09 OperatorAssignment/Main.py
+++ Modul 4/09 OperatorAssignment/Main.py
@@ -1,1 +1,51 @@
+# operasi yang dapat dilakukan dengan meringkas
 
+a = 5 # adalah assignment
+print("nilai a = ", a)
+
+a += 1 # artinya a a + 1
+print("nilai a += 1, nilai a menjadi ", a)
+
+a = 1 # artinya a = a1
+print("nilai a = 1, nilai a menjadi ", a)
+
+a *= 5 # artinya a = a 5
+print("nilai a = 1, nilai a menjadi ", a)
+
+a/= 2# artinya a a/2
+print("nilai a = 1, nilai a menjadi ", a)
+
+#modulus dan floor division
+b = 10
+print("nilai b = ", b)
+
+b %= 3# artinya b = b % 3
+print("nilai b % 3, nilai b menjadi ", b)
+
+b = 10
+print("nilai b = ", b)
+
+b //= 3 # artinya b = b // 3
+print("nilai b //= 3, nilai b menjadi", b)
+
+
+# pangkat (eksponen)
+a = 5
+print("nilai a = ", a)
+a ** 3
+print("nilai a ** 3, nilai a menjadi ", a)
+
+# bagaiman jika operasi adalah logika?
+# OR, AND
+
+# OR (1)
+c = True
+print("nilai c = ", c)
+c = False # artinya cc or False
+print("nilai c = False, nilai c menjadi ", c)
+
+# AND (&)
+c = False
+print("nilai c= ", c)
+c &= False # artinya cc and False
+print("nilai c &= False, nilai c menjadi ", c)
