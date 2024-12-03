ba75c676b19fcb0448f46a9f8ebeb1bc5f1de862
Ramadhan2212 <ramadhan72679@gmail.com>
2024-12-03 12:20:04 +0800

    Add files via upload

---
+++ Modul 4/11-FormatString/main.py
@@ -0,0 +1,57 @@
+# Format string
+# kata kunci'f' '{}'
+
+# Contoh Umum
+# tipe data string
+nama = "tsubasa ozora"
+format_str = f"Selamat Datang =(nama)"
+print(format_str)
+print(f"Selamat Datang (nama)")
+
+# tipe data boolean
+boolean = False
+format_str = f"boolean = {bool}"
+print(format_str)
+print(type(bool))
+print(type ())
+
+# angka
+angka 2005.5
+format_str =f"angka =[angka}"
+print(format_str)
+
+# bilangan bulat
+angka = 10
+format_str =f"bilangan bulat: (angka:d}"
+print(format_str)
+
+# bilangan dengan ordo ribuan
+angka = 200000 # 2,000,000
+format_str = fjutaan {angka]*
+print(format_str)
+
+# bilangan desimal
+angka 2005 54321
+format_str = f'desimal = (angka:3f}'
+print(format_str)
+
+# menampilkan tanda + atau
+angka_minus =-10
+angka_plus =+10,54321
+format_minus f'minus {angka_minus:+d}'
+format_plus = f" pllus = {angka_plus:+.2f}"
+
+print(format_minus)
+print(format_plus)
+
+# format persen
+persentase = 0.025
+format_persen = f"persen = {persentase:.2%}"
+print(format_persen)
+
+# melakukan operasi aritmatika
+harga = 5000
+jumlah = 5
+
+format_string = f"harga total = Rp. (harga jumlah:,}"
+print(format_string)
