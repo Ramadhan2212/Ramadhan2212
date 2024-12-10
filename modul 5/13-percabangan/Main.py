cdc3b43bf232a7a6cdd6da96ab7f4769d3027bdd
Ramadhan2212 <ramadhan72679@gmail.com>
2024-12-10 13:56:13 +0800

    Add files via upload

---
+++ modul 5/13-percabangan/Main.py
@@ -0,0 +1,21 @@
+# percabangan 
+# struktur
+"""
+   1. if -nya
+   2. punya kondisi
+   3. punya aksi
+"""
+nama = input("masukkan nama : ")
+
+# percabangan yang inline (satu baris)
+if nama == "adam" : print ("selamat datang")
+print("terima kasih")
+
+# percabangan indentasi
+if nama == "adam"
+   print("selamat datang")
+   print("selamat belajar python")
+print("bukan bagain percabangan")
+
+# percabangan 1 kondisi dan 2 aksi
+# pakai kata kunci 
